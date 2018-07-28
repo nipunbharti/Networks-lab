4843a60d0026984317c1884fb649de37ed32755e
nipunbharti <ced16i026@iiitdm.ac.in>
2018-07-28 20:30:04 +0530

    Added: Server and client for sockets

---
+++ Socket/server.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main()
+{
+    char server_msg[256] = "Hello from server!";
+    //Creation of socket
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    //Bind the socket to an IP
+    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+    //Listener for connections
+    listen(server_socket, 5);
+
+    int client_socket;
+    client_socket = accept(server_socket, NULL, NULL); //Null holds the address the sockets are coming from
+
+    //Send data
+    send(client_socket, server_msg, sizeof(server_msg), 0);
+
+    close(server_socket);
+    return 0;
+}
